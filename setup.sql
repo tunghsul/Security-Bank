2d7d65b6e371785db900dd4b50db53e7017ff614
Michael Nguyen <nguyen.michael.26@gmail.com>
2022-05-08 23:42:02 -0700

    Added Unique SQL name

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `users` (
     `id` serial PRIMARY KEY,
-    `name` varchar(100) NOT NULL,
+    `name` varchar(100) NOT NULL UNIQUE,
     `password` varchar(100) NOT NULL,
     `balance` int DEFAULT 0
 )
