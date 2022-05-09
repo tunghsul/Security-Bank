4beb2cf9fc0da519155c96dd6df850faa1ee353f
Michael Nguyen <nguyen.michael.26@gmail.com>
2022-05-09 00:48:18 -0700

    Update SQL Database Query

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `users` (
     `id` serial PRIMARY KEY,
-    `name` varchar(100) NOT NULL UNIQUE,
-    `password` varchar(100) NOT NULL,
-    `balance` int DEFAULT 0
+    `name` varchar(127) NOT NULL UNIQUE,
+    `password` varchar(127) NOT NULL,
+    `balance` DECIMAL(12,2) DEFAULT 0
 )
